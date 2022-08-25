87a006df740850fd4d0f84f9f084a48a180a9769
ky <originky2@gmail.com>
2022-08-25 05:09:05 -0400

    c

--- pipeline_component.py
+++ pipeline_component.py
@@ -2,7 +2,7 @@ import functools
 from typing import Callable, List, Optional
 
 
-def pipeline_component(func: Optional[Callable] = None, ):
+def pipeline_component(func: Optional[Callable] = None, ):  # decorator class connect input and output
     pass
 
 
