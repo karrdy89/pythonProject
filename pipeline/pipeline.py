1306344f4344e110815379720da6b7b32910ba81
ky <originky2@gmail.com>
2022-08-29 04:04:14 -0400

    c

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,26 +1,3 @@
-# -----
-# every module have return
-# bind ray tune (1. wrap with func and config, 2. ih tune.Trainable class) -> 1.
-# get data, model, config
-# define train function, config -> convert to ray tune with pipeline
-# is_tune on pipeline param
-# run in yaml order
-# get module from yaml (validation required), connect, run, save
-# pipeline modules must be class
-# must know module role (it provides data or model, at least know what model is for train)
-# so pipeline information must include it
-# how to implement train code? -> check codes and rebuild is unstable. give train option?
-# metric, optimizer, callback, etc..
-# or just imply train code and call fit -> same as ray tune
-# in conclusion pipeline must provide load data(involved preprocess for injection to model), model, train code
-# split all is much simple -> then split
-# but split is not necessary
-# easily merge with kubeflow is the better choice -> dig kubeflow
-# vertex Ai = kfp + def + component
-# therefore, make sdk binding kfp, distribution
-# transport data by argument each pipeline (define arg type of in, out)
-# combination of function and decorator
-
 # key concept : managed by kubeflow, provide easy GUI(aib), jupyter lab(SDK) interface between kubeflow and distribute
 # requires
 # 0. set environment and get used kfp
@@ -46,9 +23,7 @@
 
 import yaml
 import os
-import sys
 import importlib
-import inspect
 from pipeline import TrainInfo
 
 
@@ -99,12 +74,14 @@ class Pipeline:
         else:
             if not outputs:
                 if type(TrainInfo()) in inputs:
-                    print(type(train_info))
                     component(self._component_result.output, train_info)    # order checking needed
                 else:
                     component(self._component_result.output)
             else:
-                self._component_result.output = component(self._component_result.output)    # order checking needed
+                if type(TrainInfo()) in inputs:
+                    self._component_result.output = component(self._component_result.output, train_info)    # order checking needed
+                else:
+                    self._component_result.output = component(self._component_result.output)
         self._pipeline_idx += 1
         self.run_pipeline(train_info)
 
@@ -112,13 +89,5 @@ class Pipeline:
 class ComponentResult:
     def __init__(self):
         self.output = None
-
-
-
-
-
-a = Pipeline()
-a.set_pipeline('test')
-a.run_pipeline(TrainInfo())
 
 
