540b572d0efc8f4f02f606cb1964096d66b11d64
ky <originky2@gmail.com>
2022-08-29 04:02:23 -0400

    c

--- train/test.py
+++ train/test.py
@@ -6,16 +6,15 @@ def train_test_model(dataset: Input[Dataset], train_info: Input[TrainInfo]):
     import tensorflow as tf
 
     train_info = train_info
+    ds = dataset.data
+    for feat, targ in ds.take(5):
+        print('Features: {}, Target: {}'.format(feat, targ))
+    ds = ds.batch(1)
+
     model = tf.keras.Sequential([
         tf.keras.layers.Dense(1, input_shape=[1])
     ])
-    ds = dataset.data
     model.compile(optimizer="sgd", loss="mse")
-    for feat, targ in ds.take(5):
-        print('Features: {}, Target: {}'.format(feat, targ))
-
-    ds = ds.batch(1)
-
     model.fit(ds, epochs=10)
 
 
