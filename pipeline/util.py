cec0332e70fce8dfcc04128e74a8ff6f76e5663a
ky <originky2@gmail.com>
2022-09-05 01:18:06 -0400

    c

--- pipeline/util.py
+++ pipeline/util.py
@@ -1,4 +1,12 @@
 def split_ratio(data_split: str):
+    """
+    Return train, validation, test ratio of dataset from given string. (ex. 80:10:10)
+
+    Parameters
+    ----------
+        data_split : str
+            Option of dataset split ratio. (formatted string ex. 80:10:10)
+    """
     ratio = data_split.split(":")
     train = float(ratio[0])*0.01
     val = float(ratio[1])*0.01
