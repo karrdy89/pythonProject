8c390591915aac30835c611c537780093efb3e8f
ky <originky2@gmail.com>
2022-09-07 03:47:00 -0400

    c

--- db/mapper.py
+++ db/mapper.py
@@ -1,6 +1,14 @@
 import xml.etree.ElementTree as ET
+import os
 
 
 class QueryMapper:
     def __init__(self):
-        pass
+        self._path: str = os.path.dirname(os.path.abspath(__file__)) + "/query.xml"
+        self._queries = None
+        self._set_queries()
+
+    def _set_queries(self):
+        queries = ET.parse(self._path)
+        queries = queries.getroot()
+        print(queries)
