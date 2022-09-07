9591490e8f4e9df41baf3fac3c630283c1ea884e
ky <originky2@gmail.com>
2022-09-07 03:33:31 -0400

    c

---
+++ db/mapper.py
@@ -0,0 +1,6 @@
+import xml.etree.ElementTree as ET
+
+
+class QueryMapper:
+    def __init__(self):
+        pass
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,2 @@
-from .db_util import *
 from .http_util import *
 from .resettable_timer import *
