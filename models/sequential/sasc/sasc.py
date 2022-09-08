a06cd6cde9be1b6bb96cef0d616a37f01c3e20f3
ky <originky2@gmail.com>
2022-09-08 02:17:04 -0400

    c

--- models/sequential/sasc/sasc.py
+++ models/sequential/sasc/sasc.py
@@ -6,25 +6,30 @@
 # check result
 # add each stop to pipeline
 
-import pandas as pd
-import tensorflow as tf
-import matplotlib.pyplot as plt
-
+# import pandas as pd
+# import tensorflow as tf
+# import matplotlib.pyplot as plt
 
 # split each unique, copy to min idx, concat
 # use ray dataset and do sampling or sampling with skr and to file or use it or pandas -> using pandas
 # get lowest -> sep col -> concat
 # write model
-# if distribution pipe -> store(best param)
+# if distribution pipe -> store(best param) -> split train function
 
-
+import pandas as pd
 df = pd.read_csv("dataset/CJ_train.csv")
+
 df.head(5)
-u = df['Target'].unique()
-u = u.tolist()
-d = {}
-for i in u:
-    d[i] = df["Target"].value_counts()[i]
+labels = df['Target'].unique()
+labels = labels.tolist()
+samples = {}
+for i in labels:
+    samples[i] = df["Target"].value_counts()[i]
+min_sample = min(samples, key=samples.get)
+
+print(samples[2])
+# split dataset with min key
+
 
 
 
