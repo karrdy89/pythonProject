9dfa6c1edd5b34582d7c0f636edd2e7e43647374
ky <originky2@gmail.com>
2022-09-13 05:11:24 -0400

    c

--- models/sequential/sasc/modules.py
+++ models/sequential/sasc/modules.py
@@ -1,80 +1,99 @@
-import numpy as np
 import tensorflow as tf
-from tensorflow.keras.layers import Conv1D
-
-def multihead_attention(queries, keys, attention_dim: int, num_heads: int, dropout_rate: float):
-
-    # Linear projections
-    Q = tf.keras.layers.Dense(attention_dim, activation=None)(queries) # (N, T_q, C)
-    K = tf.keras.layers.Dense(attention_dim, activation=None)(keys) # (N, T_k, C)
-    V = tf.keras.layers.Dense(attention_dim, activation=None)(keys) # (N, T_k, C)
-
-    # --- MULTI HEAD ---
-    # Split and concat
-    Q_ = tf.concat(tf.split(Q, num_heads, axis=2), axis=0)  # (h*N, T_q, C/h)
-    K_ = tf.concat(tf.split(K, num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
-    V_ = tf.concat(tf.split(V, num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
+import numpy as np
 
 
-    # --- SCALED DOT PRODUCT ---
-    # Multiplication
-    outputs = tf.matmul(Q_, tf.transpose(K_, [0, 2, 1]))  # (h*N, T_q, T_k)
+class MultiHeadAttention(tf.keras.layers.Layer):
+    """
+    - Q (query), K (key) and V (value) are split into multiple heads (num_heads)
+    - each tuple (q, k, v) are fed to scaled_dot_product_attention
+    - all attention outputs are concatenated
+    """
 
-    # Scale
-    outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
+    def __init__(self, attention_dim, num_heads, dropout_rate):
+        super(MultiHeadAttention, self).__init__()
+        self.num_heads = num_heads
+        self.attention_dim = attention_dim
+        assert attention_dim % self.num_heads == 0
+        self.dropout_rate = dropout_rate
 
-    # Key Masking
-    key_masks = tf.sign(tf.abs(tf.reduce_sum(keys, axis=-1)))  # (N, T_k)
-    key_masks = tf.tile(key_masks, [num_heads, 1])  # (h*N, T_k)
-    key_masks = tf.tile(tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1])  # (h*N, T_q, T_k)
+        self.depth = attention_dim // self.num_heads
 
-    paddings = tf.ones_like(outputs) * (-2 ** 32 + 1)
-    outputs = tf.where(tf.equal(key_masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
+        self.Q = tf.keras.layers.Dense(self.attention_dim, activation=None)
+        self.K = tf.keras.layers.Dense(self.attention_dim, activation=None)
+        self.V = tf.keras.layers.Dense(self.attention_dim, activation=None)
+        self.dropout = tf.keras.layers.Dropout(self.dropout_rate)
 
-    # Future blinding (Causality)
-    diag_vals = tf.ones_like(outputs[0, :, :])  # (T_q, T_k)
-    tril = tf.linalg.LinearOperatorLowerTriangular(diag_vals).to_dense()  # (T_q, T_k)
-    masks = tf.tile(tf.expand_dims(tril, 0), [tf.shape(outputs)[0], 1, 1])  # (h*N, T_q, T_k)
+    def call(self, queries, keys):
+        # Linear projections
+        Q = self.Q(queries)  # (N, T_q, C)
+        K = self.K(keys)  # (N, T_k, C)
+        V = self.V(keys)  # (N, T_k, C)
 
-    paddings = tf.ones_like(masks) * (-2 ** 32 + 1)
-    outputs = tf.where(tf.equal(masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
+        # --- MULTI HEAD ---
+        # Split and concat
+        Q_ = tf.concat(tf.split(Q, self.num_heads, axis=2), axis=0)  # (h*N, T_q, C/h)
+        K_ = tf.concat(tf.split(K, self.num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
+        V_ = tf.concat(tf.split(V, self.num_heads, axis=2), axis=0)  # (h*N, T_k, C/h)
 
-    # Activation
-    outputs = tf.nn.softmax(outputs)  # (h*N, T_q, T_k)
+        # --- SCALED DOT PRODUCT ---
+        # Multiplication
+        outputs = tf.matmul(Q_, tf.transpose(K_, [0, 2, 1]))  # (h*N, T_q, T_k)
 
-    # Query Masking
-    query_masks = tf.sign(tf.abs(tf.reduce_sum(queries, axis=-1)))  # (N, T_q)
-    query_masks = tf.tile(query_masks, [num_heads, 1])  # (h*N, T_q)
-    query_masks = tf.tile(tf.expand_dims(query_masks, -1), [1, 1, tf.shape(keys)[1]])  # (h*N, T_q, T_k)
-    outputs *= query_masks  # broadcasting. (N, T_q, C)
+        # Scale
+        outputs = outputs / (K_.get_shape().as_list()[-1] ** 0.5)
 
-    # Dropouts
-    outputs = tf.keras.layers.Dropout(dropout_rate)(outputs)
+        # print(outputs.shape)
 
-    # Weighted sum
-    outputs = tf.matmul(outputs, V_)  # ( h*N, T_q, C/h)
+        # Key Masking
+        key_masks = tf.sign(tf.abs(tf.reduce_sum(keys, axis=-1)))  # (N, T_k)
 
-    # --- MULTI HEAD ---
-    # concat heads
-    outputs = tf.concat(tf.split(outputs, num_heads, axis=0), axis=2)  # (N, T_q, C)
+        key_masks = tf.tile(key_masks, [self.num_heads, 1])  # (h*N, T_k)
+        key_masks = tf.tile(
+            tf.expand_dims(key_masks, 1), [1, tf.shape(queries)[1], 1]
+        )  # (h*N, T_q, T_k)
 
-    # Residual connection
-    outputs += queries
+        paddings = tf.ones_like(outputs) * (-(2 ** 32) + 1)
+        outputs = tf.where(tf.equal(key_masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
 
-    return outputs
+        # Future blinding (Causality)
+        diag_vals = tf.ones_like(outputs[0, :, :])  # (T_q, T_k)
+        tril = tf.linalg.LinearOperatorLowerTriangular(
+            diag_vals
+        ).to_dense()  # (T_q, T_k)
+        masks = tf.tile(
+            tf.expand_dims(tril, 0), [tf.shape(outputs)[0], 1, 1]
+        )  # (h*N, T_q, T_k)
 
-def layer_normalization(input_seq, epsilon = 1e-8):
+        paddings = tf.ones_like(masks) * (-(2 ** 32) + 1)
+        outputs = tf.where(tf.equal(masks, 0), paddings, outputs)  # (h*N, T_q, T_k)
 
-    inputs_shape = input_seq.get_shape()
-    params_shape = inputs_shape[-1:]
+        # Activation
+        outputs = tf.nn.softmax(outputs)  # (h*N, T_q, T_k)
 
-    mean, variance = tf.nn.moments(input_seq, [-1], keep_dims=True)
-    beta = tf.zeros(params_shape)
-    gamma = tf.ones(params_shape)
-    normalized = (input_seq - mean) / ((variance + epsilon) ** .5)
-    output = gamma * normalized + beta
+        # Query Masking
+        query_masks = tf.sign(tf.abs(tf.reduce_sum(queries, axis=-1)))  # (N, T_q)
+        query_masks = tf.tile(query_masks, [self.num_heads, 1])  # (h*N, T_q)
+        query_masks = tf.tile(
+            tf.expand_dims(query_masks, -1), [1, 1, tf.shape(keys)[1]]
+        )  # (h*N, T_q, T_k)
+        outputs *= query_masks  # broadcasting. (N, T_q, C)
 
-    return output
+        # Dropouts
+        outputs = self.dropout(outputs)
+
+        # Weighted sum
+        outputs = tf.matmul(outputs, V_)  # ( h*N, T_q, C/h)
+
+        # --- MULTI HEAD ---
+        # concat heads
+        outputs = tf.concat(
+            tf.split(outputs, self.num_heads, axis=0), axis=2
+        )  # (N, T_q, C)
+
+        # Residual connection
+        outputs += queries
+
+        return outputs
 
 
 class PointWiseFeedForward(tf.keras.layers.Layer):
@@ -95,7 +114,6 @@ class PointWiseFeedForward(tf.keras.layers.Layer):
         self.dropout_layer = tf.keras.layers.Dropout(self.dropout_rate)
 
     def call(self, x):
-
         output = self.conv_layer1(x)
         output = self.dropout_layer(output)
 
@@ -108,9 +126,138 @@ class PointWiseFeedForward(tf.keras.layers.Layer):
         return output
 
 
+class EncoderLayer(tf.keras.layers.Layer):
+    """
+    Transformer based encoder layer
+    """
+
+    def __init__(
+            self,
+            seq_max_len,
+            embedding_dim,
+            attention_dim,
+            num_heads,
+            conv_dims,
+            dropout_rate,
+    ):
+        super(EncoderLayer, self).__init__()
+
+        self.seq_max_len = seq_max_len
+        self.embedding_dim = embedding_dim
+
+        self.mha = MultiHeadAttention(attention_dim, num_heads, dropout_rate)
+        self.ffn = PointWiseFeedForward(conv_dims, dropout_rate)
+
+        self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+        self.layernorm2 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
+
+        self.dropout1 = tf.keras.layers.Dropout(dropout_rate)
+        self.dropout2 = tf.keras.layers.Dropout(dropout_rate)
+
+        self.layer_normalization = LayerNormalization(
+            self.seq_max_len, self.embedding_dim, 1e-08
+        )
+
+    def call_(self, x, training, mask):
+        attn_output = self.mha(queries=self.layer_normalization(x), keys=x)
+        attn_output = self.dropout1(attn_output, training=training)
+        out1 = self.layernorm1(x + attn_output)
+
+        # feed forward network
+        ffn_output = self.ffn(out1)  # (batch_size, input_seq_len, d_model)
+        ffn_output = self.dropout2(ffn_output, training=training)
+        out2 = self.layernorm2(
+            out1 + ffn_output
+        )  # (batch_size, input_seq_len, d_model)
+
+        # masking
+        out2 *= mask
+
+        return out2
+
+    def call(self, x, training, mask):
+        x_norm = self.layer_normalization(x)
+        attn_output = self.mha(queries=x_norm, keys=x)
+        attn_output = self.ffn(attn_output)
+        out = attn_output * mask
+
+        return out
+
+
+class Encoder(tf.keras.layers.Layer):
+    """
+    Invokes Transformer based encoder with user defined number of layers
+    """
+
+    def __init__(
+            self,
+            num_layers,
+            seq_max_len,
+            embedding_dim,
+            attention_dim,
+            num_heads,
+            conv_dims,
+            dropout_rate,
+    ):
+        super(Encoder, self).__init__()
+
+        self.num_layers = num_layers
+
+        self.enc_layers = [
+            EncoderLayer(
+                seq_max_len,
+                embedding_dim,
+                attention_dim,
+                num_heads,
+                conv_dims,
+                dropout_rate,
+            )
+            for _ in range(num_layers)
+        ]
+
+        self.dropout = tf.keras.layers.Dropout(dropout_rate)
+
+    def call(self, x, training, mask):
+        for i in range(self.num_layers):
+            x = self.enc_layers[i](x, training, mask)
+
+        return x  # (batch_size, input_seq_len, d_model)
+
+
+class LayerNormalization(tf.keras.layers.Layer):
+    """
+    Layer normalization using mean and variance
+    gamma and beta are the learnable parameters
+    """
+
+    def __init__(self, seq_max_len, embedding_dim, epsilon):
+        super(LayerNormalization, self).__init__()
+        self.seq_max_len = seq_max_len
+        self.embedding_dim = embedding_dim
+        self.epsilon = epsilon
+        self.params_shape = (self.seq_max_len, self.embedding_dim)
+        g_init = tf.ones_initializer()
+        self.gamma = tf.Variable(
+            initial_value=g_init(shape=self.params_shape, dtype="float32"),
+            trainable=True,
+        )
+        b_init = tf.zeros_initializer()
+        self.beta = tf.Variable(
+            initial_value=b_init(shape=self.params_shape, dtype="float32"),
+            trainable=True,
+        )
+
+    def call(self, x):
+        mean, variance = tf.nn.moments(x, [-1], keepdims=True)
+        normalized = (x - mean) / ((variance + self.epsilon) ** 0.5)
+        output = self.gamma * normalized + self.beta
+        return output
+
+
 def embedding(input_seq, item_embedding_layer, positional_embedding_layer):
     seq_embeddings = item_embedding_layer(input_seq)
     positional_seq = tf.expand_dims(tf.range(tf.shape(input_seq)[1]), 0)
     positional_seq = tf.tile(positional_seq, [tf.shape(input_seq)[0], 1])
     positional_embeddings = positional_embedding_layer(positional_seq)
     return seq_embeddings, positional_embeddings
+
--- models/sequential/sasc/sasc.py
+++ models/sequential/sasc/sasc.py
@@ -15,6 +15,7 @@
 # if distribution pipe -> store(best param) -> split train function
 
 import pandas as pd
+import numpy as np
 from tensorflow.keras.layers.experimental import preprocessing
 from tensorflow.keras import layers
 from sklearn.model_selection import train_test_split
@@ -45,11 +46,7 @@ df.fillna('', inplace=True)
 
 y = df.pop("Target")
 df = df.iloc[:, ::-1]
-# df_list = df.values.tolist()
 X = df.stack().groupby(level=0).apply(list).tolist()
-# X = []
-# for j in df_list:
-#     X.append(j)
 
 oneCol = []
 for k in df:
@@ -77,20 +74,23 @@ y_test = label_vocab(y_test)
 vocab_size = len(le.get_vocabulary())  # Size of sequence vocab
 max_len = len(df.columns)  # Sequence length
 num_labels = len(label_vocab.get_vocabulary())
-embed_dim = 10
+embedding_dim = 64
 l2_reg = 1e-6
-dropout_rate = 0.4
+dropout_rate = 0.3
 num_blocks = 2
 attention_dim = 64
-attention_num_heads = 5
+attention_num_heads = 8
+conv_dims = [64, 64]
+epsilon = 1e-08
+learning_rate = 0.0013
 
 inputs = layers.Input(shape=(None,), name='seq', dtype=object)
 encoding_layer = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=events, mask_token=mask)
 encoded_inputs = encoding_layer(inputs)
-positional_embedding_layer = tf.keras.layers.Embedding(max_len, embed_dim,
+positional_embedding_layer = tf.keras.layers.Embedding(max_len, embedding_dim,
                                                        name='positional_embeddings', mask_zero=False,
                                                        embeddings_regularizer=tf.keras.regularizers.l2(l2_reg))
-item_embedding_layer = tf.keras.layers.Embedding(vocab_size, embed_dim,
+item_embedding_layer = tf.keras.layers.Embedding(vocab_size+1, embedding_dim,
                                                  name='item_embeddings', mask_zero=True,
                                                  embeddings_regularizer=tf.keras.regularizers.l2(l2_reg))
 seq_embeddings, positional_embeddings = embedding(encoded_inputs, item_embedding_layer, positional_embedding_layer)
@@ -99,15 +99,89 @@ mask = tf.expand_dims(tf.cast(tf.not_equal(encoded_inputs, 0), tf.float32), -1)
 seq_embeddings = tf.keras.layers.Dropout(dropout_rate)(seq_embeddings)
 seq_embeddings *= mask
 seq_attention = seq_embeddings
-for i in range(num_blocks):
-    seq_attention = multihead_attention(queries=layer_normalization(seq_attention),
-                                        keys=seq_attention,
-                                        attention_dim=attention_dim,
-                                        num_heads=attention_num_heads,
-                                        dropout_rate=dropout_rate)
-    seq_attention = point_wise_feed_forward(seq_attention, dropout_rate=dropout_rate, conv_dims=conv_dims)
-    seq_attention *= mask
+encoder = Encoder(num_blocks, max_len, embedding_dim, attention_dim, attention_num_heads, conv_dims, dropout_rate,)
+seq_attention = encoder(seq_attention, True, mask)
+layer_normalization = LayerNormalization(max_len, embedding_dim, epsilon)
+x = layer_normalization(seq_attention)  # (b, s, d)
+x = layers.GlobalAveragePooling1D()(x)
+x = layers.Dense(64, activation="relu")(x)
+x = layers.Dropout(0.1)(x)
+x = layers.Dense(16, activation="relu")(x)
+x = layers.Dropout(0.1)(x)
+outputs = layers.Dense(num_labels, activation="softmax")(x)
+model = tf.keras.Model(inputs=[inputs], outputs=outputs)
+model.summary()
+
+optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0,
+                                     amsgrad=False)
 
 
-model = tf.keras.Model(inputs=[inputs], outputs=encoded_inputs)
-model.predict([X_train[0]])
+model.compile(optimizer=optimizer, loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+history = model.fit(X_train, y_train, validation_data=(X_test,y_test),epochs=10, batch_size=64)
+
+
+
+
+
+#
+# n_hiddens = 64
+# embed_dim = 20  # Embedding size for each token
+# # inputs = layers.Input(shape=(max_len,), name='seq')
+# inputs = layers.Input(shape=(None,), name='seq', dtype=object)
+# encoding_layer = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=events, mask_token=mask)
+# encoded_inputs = encoding_layer(inputs)
+# x1 = tf.keras.layers.Embedding(vocab_size+1, embed_dim, input_length=max_len, mask_zero=True)(encoded_inputs)
+# x1 = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(n_hiddens))(x1)
+# x1 = tf.keras.layers.Dense(num_labels, activation='softmax', name='output')(x1)
+# lstm_model = tf.keras.models.Model(inputs=[inputs], outputs=x1)
+# lstm_model.summary()
+# lstm_model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['acc'])
+# lstm_history = lstm_model.fit(X_train, y_train, validation_data=(X_test,y_test), epochs=5, batch_size=32)
+
+
+#
+# from models.sequential.sasc.modules_2 import *
+# from tensorflow.python.keras.regularizers import l2
+#
+# num_layers = 2  # Number of encoding layer
+# dropout = 0.3  # dropout rate of embedding layer
+# embed_dim = 20  # Embedding size for each token
+# num_heads = 5  # Number of attention heads
+# ff_dim = 64  # Hidden layer size in feed forward network inside transformer
+# learning_rate = 0.0013  # Learning rate of optimizer
+#
+#
+# inputs = layers.Input(shape=(None,), name='seq', dtype=object)
+# padding_layer = PaddingLayer(maxlen=max_len, name='padding')
+# padding_input = padding_layer(inputs)
+# encoding_layer = tf.keras.layers.experimental.preprocessing.StringLookup(vocabulary=events)
+# encoded_inputs = encoding_layer(padding_input)
+# embeddings = tf.keras.layers.Embedding(vocab_size, embed_dim, input_length=max_len, embeddings_initializer='random_uniform', embeddings_regularizer=l2(1e-6))(encoded_inputs)
+# embeddings *= tf.math.sqrt(tf.cast(embed_dim, tf.float32))
+# embeddings = PositionalEncoding(vocab_size, embed_dim)(embeddings)
+# embeddings = tf.keras.layers.Dropout(rate=dropout)(embeddings)
+# enc_padding_mask = tf.keras.layers.Lambda(create_padding_mask, output_shape=(1,1,None), name='enc_padding_mask')(encoded_inputs)
+# transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+# x = transformer_block(embeddings, enc_padding_mask)
+# for i in range(num_layers-1):
+#     x = transformer_block(x, enc_padding_mask)
+# x = layers.GlobalAveragePooling1D()(x)
+# x = layers.Dropout(0.1)(x)
+# x = layers.Dense(64, activation="relu")(x)
+# x = layers.Dense(16, activation="relu")(x)
+# x = layers.Dropout(0.1)(x)
+# outputs = layers.Dense(num_labels, activation="softmax")(x)
+# model = tf.keras.Model(inputs=[inputs], outputs=outputs)
+# model.summary()
+#
+# optimizer = tf.keras.optimizers.Adam(lr=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+#
+# def loss_function(y_true, y_pred):
+#   loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')(y_true, y_pred)
+#   mask = tf.cast(tf.not_equal(y_true, 0), tf.float32)
+#   loss = tf.multiply(loss, mask)
+#   return tf.reduce_mean(loss)
+#
+#
+# model.compile(optimizer=optimizer, loss=loss_function, metrics=['accuracy'])
+# history = model.fit(X_train, y_train, validation_data=(X_test,y_test),epochs=3, batch_size=64)
