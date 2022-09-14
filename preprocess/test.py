267f2429929ebac58e1b079e58604551102044a4
ky <originky2@gmail.com>
2022-09-13 21:11:52 -0400

    c

--- preprocess/test.py
+++ preprocess/test.py
@@ -2,25 +2,13 @@ from pipeline import Input, Output, Dataset, PipelineComponent
 
 
 @PipelineComponent
-def process_nbo_data(input_data: Input[Dataset]) -> Output[Dataset]:
-    import pandas as pd
+def process_test_data(input_data: Input[Dataset]) -> Output[Dataset]:
+    import tensorflow as tf
     input_data = input_data.data
-    labels = input_data['Target'].unique()
-    labels = labels.tolist()
-    samples = {}
-    for i in labels:
-        samples[i] = input_data["Target"].value_counts()[i]
-    min_sample = min(samples, key=samples.get)
-    sep_frames = []
-    for label in labels:
-        if label is not min_sample:
-            downed = input_data[input_data['Target'] == label]
-            downed = downed.sample(n=samples[min_sample].item(), random_state=0)
-            sep_frames.append(downed)
-    df = pd.concat(sep_frames, axis=0)
-    df.fillna('', inplace=True)
+    target = input_data.pop('target')
+    dataset = tf.data.Dataset.from_tensor_slices((input_data.values, target.values))
     out = Dataset()
-    out.framework = 'pd'
+    out.framework = 'tf'
     out.length = len(input_data.index)
-    out.data = df
+    out.data = dataset
     return out
