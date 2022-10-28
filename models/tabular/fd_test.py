df465a2a7a3444d8392b298dbdd61b70fb394975
ky <originky2@gmail.com>
2022-10-28 14:29:20 +0900

    d

---
+++ models/tabular/fd_test.py
@@ -0,0 +1,31 @@
+# read dataset
+
+import pandas as pd
+from statics import ROOT_DIR
+
+dataset_path = ROOT_DIR + "/dataset/fd_test/fd_dataset.csv"
+df = pd.read_csv(dataset_path)
+print(df.keys())
+df = df[["MID", "IP", "PID", "PR", "Class"]]
+# CCM -> CCW check it is good to go
+df["MID"] = df["MID"].str.replace("CCW", "CCM")
+df[["PID"]] = df[["PID"]].fillna(value="NA")
+df["EVT"] = df.MID.str.cat(df.PID, sep='-')
+
+# uniques to col
+events = df["EVT"].unique().tolist()
+len(events)
+
+# sep with ip
+ips = df["IP"].unique().tolist()
+dfs = []
+for ip in ips:
+    dfs.append(df[df['IP'] == ip])
+
+# start with login(MID) if class == 0 end with PR is not 0, else to end of ip from sep
+login_event = "CCMLO0101"
+for df in dfs:
+    pass
+
+# print(df.values.tolist())
+
