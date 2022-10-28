6b2a38512570eb89d63683d2240a2e55bb1b7ed8
ky <originky2@gmail.com>
2022-10-28 16:20:26 +0900

    d

--- models/tabular/fd_test.py
+++ models/tabular/fd_test.py
@@ -24,18 +24,27 @@ len(events)
 ips = df["IP"].unique().tolist()
 tdfs = []
 for ip in ips:
-    tdfs.append(df[df['IP'] == ip].reset_index())
+    tdfs.append(df[df['IP'] == ip].reset_index(drop=True))
 
 login_event = "CCMLO0101"
+
+dataset = pd.DataFrame(columns=["IP"] + events + ["label"])
 
 for tdf in tdfs:
     ip = tdf["IP"].iloc[0]
     label = tdf["Class"].iloc[0]
     start_idxes = tdf.index[tdf["MID"] == login_event].tolist()
     end_idxes = tdf.index[tdf["PR"] != 0].tolist()
+    data_row = {"IP": ip, "label": label}
+    start_idx = start_idxes[0]
     for end_idx in end_idxes:
-        pass
+        temp_df = tdf[start_idx:end_idx+1]
+        counted_items = (dict(temp_df["EVT"].value_counts()))
+        data_row.update(counted_items)
+        data_row = pd.DataFrame([data_row])
+        dataset = pd.concat([dataset, data_row], ignore_index=True)
+        print(dataset)
+        break
     print(start_idxes, start_idxes)
 
 # print(df.values.tolist())
-
