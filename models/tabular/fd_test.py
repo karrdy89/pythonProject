d88b631138c5a40d3affa071d32bd23d353bb4b6
ky <originky2@gmail.com>
2022-10-28 17:07:29 +0900

    d

--- models/tabular/fd_test.py
+++ models/tabular/fd_test.py
@@ -26,25 +26,36 @@ tdfs = []
 for ip in ips:
     tdfs.append(df[df['IP'] == ip].reset_index(drop=True))
 
-login_event = "CCMLO0101"
-
+begin_event = "CCMLO0101"   # login
 dataset = pd.DataFrame(columns=["IP"] + events + ["label"])
 
 for tdf in tdfs:
     ip = tdf["IP"].iloc[0]
     label = tdf["Class"].iloc[0]
-    start_idxes = tdf.index[tdf["MID"] == login_event].tolist()
+    start_idxes = tdf.index[tdf["MID"] == begin_event].tolist()
     end_idxes = tdf.index[tdf["PR"] != 0].tolist()
-    data_row = {"IP": ip, "label": label}
     start_idx = start_idxes[0]
-    for end_idx in end_idxes:
-        temp_df = tdf[start_idx:end_idx+1]
+    if len(start_idxes) == 0:
+        continue
+    if len(end_idxes) == 0:
+        temp_df = tdf[start_idx:]
         counted_items = (dict(temp_df["EVT"].value_counts()))
+        data_row = {"IP": ip, "label": label}
         data_row.update(counted_items)
         data_row = pd.DataFrame([data_row])
         dataset = pd.concat([dataset, data_row], ignore_index=True)
-        print(dataset)
-        break
-    print(start_idxes, start_idxes)
+    else:
+        for end_idx in end_idxes:
+            temp_df = tdf[start_idx:end_idx+1]
+            counted_items = (dict(temp_df["EVT"].value_counts()))
+            data_row = {"IP": ip, "label": label}
+            data_row.update(counted_items)
+            data_row = pd.DataFrame([data_row])
+            dataset = pd.concat([dataset, data_row], ignore_index=True)
+            for it_start_idx in start_idxes:
+                if it_start_idx > end_idx+1:
+                    start_idx = it_start_idx
 
-# print(df.values.tolist())
+# dataset.to_csv(ROOT_DIR + "/dataset/fd_test/fd_dataset_tabular.csv", sep=",", index=False)
+
+
