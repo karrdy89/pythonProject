8442b7a4edc2caa00e1ab35487368b9ec29ca5b3
ky <originky2@gmail.com>
2022-10-31 17:38:33 +0900

    d

--- models/tabular/fd_test.py
+++ models/tabular/fd_test.py
@@ -59,35 +59,37 @@
 # dataset.to_csv(ROOT_DIR + "/dataset/fd_test/fd_dataset_tabular.csv", na_rep=0, sep=",", index=False)
 
 
-import pandas as pd
-import numpy as np
-from statics import ROOT_DIR
+# make model
 
-dataset_path = ROOT_DIR + "/dataset/fd_test/fd_dataset_tabular.csv"
-df = pd.read_csv(dataset_path)
-df_labels = df[["label"]]
-df.drop(["label", "IP"], axis=1, inplace=True)
-
-
-def convert_to_float(collection):
-    floats = [float(el) for el in collection]
-    return np.array(floats)
-
-df_numeric = pd.concat([df.apply(convert_to_float)], axis=1)
-df_numeric = df_numeric.values.tolist()
-df_numeric = np.array(df_numeric)
-
-
-# projection 2d
-from sklearn.manifold import TSNE
-
-model = TSNE(n_components=2, perplexity=1, n_iter=3000, learning_rate=200, init="pca")
-res = model.fit_transform(df_numeric)
-
-import matplotlib.pyplot as plt
-
-plt.scatter(res[:, 0], res[:, 1])
-plt.show()
+# import pandas as pd
+# import numpy as np
+# from statics import ROOT_DIR
+#
+# dataset_path = ROOT_DIR + "/dataset/fd_test/fd_dataset_tabular.csv"
+# df = pd.read_csv(dataset_path)
+# df_labels = df[["label"]]
+# df.drop(["label", "IP"], axis=1, inplace=True)
+#
+#
+# def convert_to_float(collection):
+#     floats = [float(el) for el in collection]
+#     return np.array(floats)
+#
+# df_numeric = pd.concat([df.apply(convert_to_float)], axis=1)
+# df_numeric = df_numeric.values.tolist()
+# df_numeric = np.array(df_numeric)
+#
+#
+# # projection 2d
+# from sklearn.manifold import TSNE
+#
+# model = TSNE(n_components=2, perplexity=1, n_iter=3000, learning_rate=200, init="pca")
+# res = model.fit_transform(df_numeric)
+#
+# import matplotlib.pyplot as plt
+#
+# plt.scatter(res[:, 0], res[:, 1])
+# plt.show()
 
 # smote
 from imblearn.over_sampling import SMOTE #pip install imbalanced-learn
@@ -100,3 +102,19 @@ from imblearn.over_sampling import SMOTE #pip install imbalanced-learn
 # use PCA or LDA
 # use t-sne to visualization
 # use under bagging random forest
+
+# build logics with sample dataset
+from collections import Counter
+from sklearn.datasets import make_classification
+from imblearn.over_sampling import SMOTE
+
+X, y = make_classification(n_classes=2, class_sep=2, weights=[0.1, 0.9], n_informative=3, n_redundant=1, flip_y=0,
+                           n_features=50, n_clusters_per_class=1, n_samples=1000, random_state=10)
+
+# visualization
+print('Original dataset shape %s' % Counter(y))
+
+
+sm = SMOTE(random_state=42)
+X_res, y_res = sm.fit_resample(X, y)
+print('Resampled dataset shape %s' % Counter(y_res))
