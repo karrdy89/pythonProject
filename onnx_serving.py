cb5dcf68aa1af7c2033690e0f8676e8d557d285e
ky <originky2@gmail.com>
2022-11-02 15:14:05 +0900

    d

--- onnx_serving.py
+++ onnx_serving.py
@@ -7,12 +7,15 @@ class OnnxServing:
     def __init__(self):
         self._worker = type(self).__name__
 
+    def init(self) -> int:
+        return 0
+
     def _load_model(self):
         pass
 
 
 # load model
 # set attribute
-# define input shape and type
-# define output type
+# define input shape and type from metadata
+# define output type from metadata(label dict)
 # serv with predict method
