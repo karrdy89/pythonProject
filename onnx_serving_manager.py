845388ccef5f4bd35c87ea3786fc4d869534c242
ky <originky2@gmail.com>
2022-11-02 17:08:49 +0900

    d

--- onnx_serving_manager.py
+++ onnx_serving_manager.py
@@ -1,3 +1,17 @@
+import configparser
+import os
+import asyncio
+import shutil
+import uuid
+import functools
+import logging
+import logger
+from dataclasses import dataclass, field
+from shutil import copytree, rmtree
+from concurrent.futures import ThreadPoolExecutor
+from itertools import cycle
+from typing import Optional
+
 import ray
 
 
@@ -6,6 +20,8 @@ class OnnxServingManager:
     def __init__(self):
         self._worker: str = type(self).__name__
 
+    def init(self):
+        pass
 
 # manage actor like tf container
 # method : deploy
@@ -18,3 +34,32 @@ class OnnxServingManager:
 # merge with tf serving? issue is max container num, init process, manage point
 # only advantage is performance
 
+
+
+@dataclass
+class ServingActor:
+    name: str
+    actor: ray.actor
+    state: int
+    ref_count: int = 0
+
+
+@dataclass
+class ModelDeployState:
+    model: tuple[str, int]
+    state: int
+    cycle_iterator = None
+    actors: dict = field(default_factory=dict)
+
+
+class StateCode:
+    ALREADY_EXIST = 1
+    IN_PROGRESS = 3
+    AVAILABLE = 0
+    UN_AVAILABLE = -1
+    SHUTDOWN = 4
+
+
+class ManageType:
+    MODEL = 0
+    CONTAINER = 1
