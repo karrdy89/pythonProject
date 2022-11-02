0f4b4cefa6cd634be451ee31816c6adb051e60f4
ky <originky2@gmail.com>
2022-11-02 15:34:29 +0900

    d

--- onnx_serving.py
+++ onnx_serving.py
@@ -1,11 +1,13 @@
 import ray
 from ray import actor
+from statics import ROOT_DIR
 
 
 @ray.remote
 class OnnxServing:
     def __init__(self):
-        self._worker = type(self).__name__
+        self._worker: str = type(self).__name__
+        self._model_path: str = ""
 
     def init(self, model_id: str, version: str) -> int:
         return 0
--- train/FD.py
+++ train/FD.py
@@ -1,6 +1,12 @@
 from pipeline import Input, TrainInfo, PipelineComponent
+from statics import ROOT_DIR
 
 
 @PipelineComponent
 def train_FD_model(train_info: Input[TrainInfo]):
-    pass
+    sp_model_info = train_info.name.split(':')
+    model_name = sp_model_info[0]
+    mn_version = sp_model_info[-1].split('.')[0]
+    # base_dataset_path = ROOT_DIR + "/dataset/" + model_name + "/" + mn_version + "/"
+    test_dataset_path = ROOT_DIR + "/dataset/fd_test/td_dataset_tabular.csv"
+
