5cb7498957f56402345220844b4b3df20d4caa6d
ky <originky2@gmail.com>
2022-11-02 13:43:29 +0900

    d

--- models/tabular/fd_test.py
+++ models/tabular/fd_test.py
@@ -1,9 +1,10 @@
 # # make dataset
 #
 # import pandas as pd
-# from statics import ROOT_DIR
-#
+from statics import ROOT_DIR
+
 # dataset_path = ROOT_DIR + "/dataset/fd_test/fd_dataset.csv"
+saved_model_path = ROOT_DIR + "/saved_models/td_test/"
 # df = pd.read_csv(dataset_path)
 # print(df.keys())
 # df = df[["MID", "IP", "PID", "PR", "Class"]]
@@ -187,34 +188,117 @@ print('Resampled dataset shape %s' % Counter(y_res))
 # plt.show()
 
 # random forest with smote
-from numpy import mean
-from sklearn.model_selection import cross_val_score, train_test_split, cross_validate
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.pipeline import Pipeline
-
-# define model
-model = RandomForestClassifier(n_estimators=10)
-# define evaluation procedure
-pipe = Pipeline([('scaler', StandardScaler()),
-                 ('rf_classifier', model)])
-
-cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
-# evaluate model
-cv_result = cross_validate(pipe, X_res, y_res, scoring='roc_auc', cv=cv, n_jobs=1, return_estimator=True,
-                           return_train_score=True)
-print(cv_result["estimator"])
-print(cv_result["train_score"])
-print(cv_result["test_score"])
-# summarize performance
-print('Mean Train ROC AUC: %.3f' % mean(cv_result["train_score"]))
-est_0 = cv_result["estimator"][0]
-est_0.score(X, y)
-# export to onnx
-
-
+# from numpy import mean
+# from sklearn.model_selection import cross_val_score, train_test_split, cross_validate
+# from sklearn.model_selection import RepeatedStratifiedKFold
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.pipeline import Pipeline
+#
+# # define model
+# model = RandomForestClassifier(n_estimators=10)
+# # define evaluation procedure
+# pipe = Pipeline([('scaler', StandardScaler()),
+#                  ('rf_classifier', model)])
+#
+# cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+# # evaluate model
+# cv_result = cross_validate(pipe, X_res, y_res, scoring='roc_auc', cv=cv, n_jobs=1, return_estimator=True,
+#                            return_train_score=True)
+# # print(cv_result["estimator"])
+# # print(cv_result["train_score"])
+# # print(cv_result["test_score"])
+# # summarize performance
+# print('Mean Train ROC AUC: %.3f' % mean(cv_result["train_score"]))
+# est_0 = cv_result["estimator"][0]
+# est_0.score(X, y)
+# # export to onnx
+# from skl2onnx import convert_sklearn
+# from skl2onnx.common.data_types import FloatTensorType
+# model_onnx = convert_sklearn(est_0, 'pipeline_estimator',
+#                 [('input', FloatTensorType([None, n_features]))])
+#
+# import os
+# if not os.path.exists(saved_model_path):
+#     os.makedirs(saved_model_path)
+#
+# with open(saved_model_path + "ft_test_random_frest.onnx", "wb") as f:
+#     f.write(model_onnx.SerializeToString())
+#
+# import onnxruntime as rt    # pip install onnxruntime
+# print("predict", est_0.predict(X[:5]))
+# print("predict_proba", est_0.predict_proba(X[:5]))
+#
+# sess = rt.InferenceSession(saved_model_path + "ft_test_random_frest.onnx")
+# pred_onx = sess.run(None, {"input": X[:5].astype(np.float32)})
+# print("predict", pred_onx[0])
+# print("predict_proba", pred_onx[1][:1])
+#
 
 # XGBoost with smote
+from numpy import mean
+from sklearn.datasets import make_classification
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import RepeatedStratifiedKFold
+from xgboost import XGBClassifier   #pip install xgboost
+from sklearn.model_selection import GridSearchCV
+from skl2onnx.common.data_types import FloatTensorType
+
+model = XGBClassifier()
+weights = [1, 10, 25, 50, 75, 99, 100, 1000]
+param_grid = dict(scale_pos_weight=weights)
+# define evaluation procedure
+cv = RepeatedStratifiedKFold(n_splits=10, n_repeats=3, random_state=1)
+grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=1, cv=cv, scoring='roc_auc')
+# execute the grid search
+grid_result = grid.fit(X, y)
+# report the best configuration
+print("Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
+# report all configurations
+means = grid_result.cv_results_['mean_test_score']
+stds = grid_result.cv_results_['std_test_score']
+params = grid_result.cv_results_['params']
+for mean, stdev, param in zip(means, stds, params):
+    print("%f (%f) with: %r" % (mean, stdev, param))
+
+from sklearn.pipeline import Pipeline
+pipe = Pipeline([('scaler', StandardScaler()),
+                 ('XGB_Classifier', model)])
+pipe.fit(X, y)
+
+from skl2onnx import convert_sklearn, update_registered_converter
+from skl2onnx.common.shape_calculator import calculate_linear_classifier_output_shapes
+from onnxmltools.convert.xgboost.operator_converters.XGBoost import convert_xgboost # pip install onnxmltools
+
+update_registered_converter(
+    XGBClassifier, 'XGBoostXGBClassifier',
+    calculate_linear_classifier_output_shapes, convert_xgboost,
+    options={'nocl': [True, False], 'zipmap': [True, False, 'columns']})
+
+model_onnx = convert_sklearn(pipe, 'pipeline_xgb',
+                [('input', FloatTensorType([None, n_features]))])
+
+import os
+if not os.path.exists(saved_model_path):
+    os.makedirs(saved_model_path)
+
+with open(saved_model_path + "ft_test_xgboost.onnx", "wb") as f:
+    f.write(model_onnx.SerializeToString())
+
+print("predict", pipe.predict(X[:5]))
+print("predict_proba", pipe.predict_proba(X[:1]))
+
+import onnxruntime as rt
+sess = rt.InferenceSession(saved_model_path + "ft_test_xgboost.onnx")
+pred_onx = sess.run(None, {"input": X[:5].astype(np.float32)})
+print("predict", pred_onx[0])
+print("predict_proba", pred_onx[1][:1])
+
+
+# evaluate model
+# scores = cross_val_score(model, X_res, y_res, scoring='roc_auc', cv=cv, n_jobs=1)
+# summarize performance
+# print('Mean ROC AUC: %.5f' % mean(scores))
+
 
 # under bagging with imb data
 
