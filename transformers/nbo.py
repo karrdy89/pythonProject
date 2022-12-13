13246f637831d3d0907f24a90b6f92855c4b9f15
ky <originky2@gmail.com>
2022-12-13 13:01:59 +0900

    d

--- transformers/nbo.py
+++ transformers/nbo.py
@@ -5,7 +5,7 @@ from db.db_util import DBUtil
 
 def transform_data(data: list, max_len: int | None = None):
     db = DBUtil(db_info="MANAGE_DB", concurrency=False)
-    events = db.select(name="select_nbo_event_test", param={"CUST_NO": data[0]})
+    events = db.select(name="select_nbo_event", param={"CUST_NO": data[0]})
     if max_len:
         events = np.array(events[:max_len])
     events = np.ravel(events, order="C")
